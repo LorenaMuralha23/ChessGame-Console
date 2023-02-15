75838c16177a73800d224060af793df444720004
Lorena Muralha <mlmuralha2301@gmail.com>
2023-02-15 19:38:12 -0300

    Position class created

---
+++ xadrez-console/chessboard/Position.cs
@@ -0,0 +1,21 @@
+﻿namespace chessboard;
+{
+    class Position
+    {
+
+        public int line { get; set; }
+        public int column { get; set; }
+
+        public Position(int linha, int coluna)
+        {
+            this.line = linha;
+            this.column = coluna;
+        }
+
+        public override string ToString()
+        {
+            return line + ", "+ column;
+        }
+
+    }
+}
